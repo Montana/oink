4cb5da1857c4d9390bf1f0146303309897787bae
Montana Mendy <montanamendy@gmail.com>
2016-09-08 17:51:12 -0700

    Update trump_tweets.py

--- trump_tweets.py
+++ trump_tweets.py
@@ -163,5 +163,5 @@ if __name__ == '__main__':
         twit = TwitterSearchImpl(0, 5, writer)
         twit.search("from:realdonaldtrump") 
         
-        # You can change "realdonaldtrump" to any twitter account, so in essence, you can respond to OKC messages from your own Twitter account if you desire.
+        # You can change "realdonaldtrump" to any twitter account.
         
